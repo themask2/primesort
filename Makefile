bca02cccc9c57cfec9c98cedeed769537029403f
themask2 <deny.stanley.06@gmail.com>
2017-11-05 16:53:50 -0200

    funcionando num i3 2.3GHz de 2 núcleos

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Macros para compilacao
 CC = gcc
-CFLAGS = -Wextra
+CFLAGS = -Wextra -lm
 DIR = src
 FILENAME = $(DIR)/main.c
 TARGET = ./main
--- src/main.c
+++ src/main.c
@@ -1,17 +1,71 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+/* PrimeSort 
+ * Deny Stanley de Oliveira Barbosa		RA:145851
  */
 
 #include <stdio.h>
+#include <math.h>
+
+int testa_numero(int num); /*Se numero e primo retorna 1, senao retorna 0 */
+
 
 int main() {
 
-  int x, y;
+	int n = 1, teste, i = 0, j;
+	int naoprimos[200000];
+	FILE *out, *arquivo;
+	char ch;
+	arquivo = fopen("ra145851.txt", "w");
+	fclose(arquivo);
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+	while (n != (-1)) {
+		scanf(" %d", &n);
+		teste = testa_numero(n);
+		if (!teste) { /* Se não primo*/
+			//arquivo = fopen("ra145851.txt", "a"); /*abre o arquivo e escreve ao final do arquivo*/
+			//fprintf(arquivo,"%d\n", n); /*escreve o numero nao primo no arquivo*/
+			//fclose(arquivo); /*fecha o arquivo*/
+			//printf("%d\n", n);
+			//out = popen("cat ra145851.txt | sort --g -r > ra145851_1.txt", "r");   /*consome (ordenando) oss valores produzidos acima*/
+			//pclose(out);
+			naoprimos[i] = n;
+			i++;
+		}
+	}	
+	arquivo = fopen("ra145851.txt", "a");
+	for (j = i - 1; j >= 0; j--) {
+		fprintf(arquivo,"%d\n", naoprimos[j]);	
+	}
+	fclose(arquivo);
+
+	out = popen("cat ra145851.txt | sort --g -r > ra145851_1.txt", "r");
+	pclose(out);
+	/*imprimindo na tela*/
+	out = popen("cat ra145851_1.txt", "r");
+	while( (ch=fgetc(out))!= EOF )
+		putchar(ch);
+	pclose(out);
+	return 0;
+}
+
+int testa_numero(int num){
+	int i, counter = 1; 
+	if (num == 1) {
+		return 0; /*numero 1 nao e primo*/
+	} else {
+		if (num == 2) {
+			return 1;
+		} else {
+			for (i = 1; i <= ceil(sqrt(num)); i++) {
+				if (num % i == 0) {
+					counter++;
+				}
+			}
+
+			if (counter > 2) {  /*Se tiver mais de 2 divisores é não primo*/
+				return 0;
+			} else {
+				return 1; /*é primo*/
+			}
+		}
+	}
 }
